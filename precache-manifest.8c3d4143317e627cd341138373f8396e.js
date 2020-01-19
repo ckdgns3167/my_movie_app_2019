dcd85554d26ba0f81e3aba8b91e7c8715b360b84
newckdgns <newckdgns@naver.com>
2020-01-19 11:08:51 +0900

    Updates

---
+++ precache-manifest.8c3d4143317e627cd341138373f8396e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98798731275721ee8b56d386cdc3f89b",
+    "url": "/my_movie_app_2019/index.html"
+  },
+  {
+    "revision": "a19df0f516812e142181",
+    "url": "/my_movie_app_2019/static/css/main.07261364.chunk.css"
+  },
+  {
+    "revision": "364f4e6779aeb1dfee8c",
+    "url": "/my_movie_app_2019/static/js/2.56246f7d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my_movie_app_2019/static/js/2.56246f7d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a19df0f516812e142181",
+    "url": "/my_movie_app_2019/static/js/main.c7b837b8.chunk.js"
+  },
+  {
+    "revision": "68ddb5e0c1137f246620",
+    "url": "/my_movie_app_2019/static/js/runtime-main.6cf33fe4.js"
+  }
+]);
